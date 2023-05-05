c2e61d4f5984d7066d027697082b26c946ae6763
Valerio <valeriorocca999@gmail.com>
2023-05-05 15:47:59 +0200

    creation w and w_bar

--- Optimization_HW1.py
+++ Optimization_HW1.py
@@ -12,14 +12,14 @@ sigma1 = np.array([[7, 0], [0, 0.5]])
 sigma2 = np.array([[0.5, 0], [0, 4]])  # Modified standard deviation for sample 2
 sigma3 = np.array([[7, 0], [0, 0.5]])
 sigma4 = np.array([[0.5, 0], [0, 4]])  # Modified standard deviation for sample 4
-weights = [0.5, 0.5]
+weights = [0.25, 0.25, 0.25, 0.25]
 
 # Generate random points in the L shape using the Bivariate Gaussian Mixture Distribution
-n_points = 10000
+n_points = 1000 #change to 10000
 samples1 = np.random.multivariate_normal(mu1, sigma1, int(n_points*weights[0]))
 samples2 = np.random.multivariate_normal(mu2, sigma2, int(n_points*weights[1]))
-samples3 = np.random.multivariate_normal(mu3, sigma3, int(n_points*weights[0]))
-samples4 = np.random.multivariate_normal(mu4, sigma4, int(n_points*weights[1]))
+samples3 = np.random.multivariate_normal(mu3, sigma3, int(n_points*weights[2]))
+samples4 = np.random.multivariate_normal(mu4, sigma4, int(n_points*weights[3]))
 
 # Assign class labels to the samples
 # class1 = np.concatenate((samples2, samples1))
@@ -48,19 +48,22 @@ unlabeled_samples = all_samples[all_samples[:,2] == 0]
 class1_samples = all_samples[all_samples[:,2] == 1]
 class2_samples = all_samples[all_samples[:,2] == -1]
 
+# Labeled matrix
+labeled_samples = np.concatenate((class1_samples, class2_samples))
+
 # Plot the samples of each class with a different color and marker
-plt.scatter(unlabeled_samples[:,0], unlabeled_samples[:,1], color='grey', label='Unlabeled', alpha=0.5)
-plt.scatter(class1_samples[:,0], class1_samples[:,1], color='red', label='Label 1', alpha=0.5)
-plt.scatter(class2_samples[:,0], class2_samples[:,1], color='blue', label='Label -1', alpha=0.5)
+#plt.scatter(unlabeled_samples[:,0], unlabeled_samples[:,1], color='grey', label='Unlabeled', alpha=0.5)
+#plt.scatter(class1_samples[:,0], class1_samples[:,1], color='red', label='Label 1', alpha=0.5)
+#plt.scatter(class2_samples[:,0], class2_samples[:,1], color='blue', label='Label -1', alpha=0.5)
 
 
 # Add legend and axis labels
-plt.legend()
-plt.xlabel('X')
-plt.ylabel('Y')
+#plt.legend()
+#plt.xlabel('X')
+#plt.ylabel('Y')
 
 # Show the plot
-plt.show()
+#plt.show()
 
 # -------------------------------------
 
@@ -70,6 +73,22 @@ plt.show()
 #distance = np.linalg.norm(a-b)
 #print(distance)
 
+w = np.zeros((np.shape(unlabeled_samples)[0], np.shape(labeled_samples)[0]))
+w_bar = np.zeros((np.shape(unlabeled_samples)[0], np.shape(unlabeled_samples)[0]))
 
-# -------------------------------------
+# similarity matrix unlabeled-labeled
+for row in range(np.shape(w)[0]):
+  for col in range(np.shape(w)[1]):
+    w[row,col] = np.linalg.norm(unlabeled_samples[row,0:1] - labeled_samples[col,0:1])
+
+# similarity matrix unlabeled-unlabeled
+for row in range(np.shape(w_bar)[0]):
+  for col in range(np.shape(w_bar)[1]):
+    w_bar[row,col] = np.linalg.norm(unlabeled_samples[row,0:1] - unlabeled_samples[col,0:1])
+
+
+#-----
+# Part 4: 
+
+
 
